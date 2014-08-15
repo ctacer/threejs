eaa04ce51145644e51de817c192ba86bf0b2b56f
Святослав <ctacik01@gmail.com>
2014-08-15 17:31:16 +0300

    created cube at the sky

--- startup/app/js/app.js
+++ startup/app/js/app.js
@@ -3,11 +3,52 @@ $.ready(function () {
 
   "use strict";
 
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+  function makeSkybox( urls, size ) {
+    var skyboxCubemap = THREE.ImageUtils.loadTextureCube( urls );
+    skyboxCubemap.format = THREE.RGBFormat;
 
-  var renderer = new THREE.WebGLRenderer();
+    var skyboxShader = THREE.ShaderLib['cube'];
+    skyboxShader.uniforms['tCube'].value = skyboxCubemap;
+
+    return new THREE.Mesh(
+      new THREE.BoxGeometry( size, size, size ),
+      new THREE.ShaderMaterial({
+        fragmentShader : skyboxShader.fragmentShader, vertexShader : skyboxShader.vertexShader,
+        uniforms : skyboxShader.uniforms, depthWrite : false, side : THREE.BackSide
+      })
+    );
+  };
+
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 9000);
+
+  var renderer = new THREE.WebGLRenderer({ antialias : true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   $.append(renderer.domElement);
+
+
+  var geometry = new THREE.BoxGeometry(5, 5, 5);
+  var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  var cube = new THREE.Mesh(geometry, material);
+  scene.add( cube );
+
+  scene.add( makeSkybox( [
+      'textures/skybox/px.jpg', // right
+      'textures/skybox/nx.jpg', // left
+      'textures/skybox/py.jpg', // top
+      'textures/skybox/ny.jpg', // bottom
+      'textures/skybox/pz.jpg', // back
+      'textures/skybox/nz.jpg'  // front
+    ], 8000 ));
+
+  camera.position.z = 50;
+
+  function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+    cube.rotation.x += 0.1;
+    // cube.rotation.y += 0.1;
+  }
+  render();
 
 });
